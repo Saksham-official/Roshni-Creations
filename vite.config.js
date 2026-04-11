030e336ce3b1ee31abb2464b41b2abbb83a6ba91
Saksham <saksham.goel28@gmail.com>
2026-04-11 18:17:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import expressApp from './api/index.js'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'express-plugin',
+      configureServer(server) {
+        server.middlewares.use(expressApp);
+      }
+    }
+  ],
 })
